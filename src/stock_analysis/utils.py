0b49b417887ba3d6a4cbde94ea0a12b09d22a00c
ryanalvita <ryanalvita@yahoo.com>
2023-04-16 21:35:43 +0700

    Adjust utils

--- src/stock_analysis/utils.py
+++ src/stock_analysis/utils.py
@@ -1,8 +1,12 @@
 import numpy as np
+import pandas as pd
+
 
 def clean_dataframe(df):
     for index in df.index:
         df.loc[index] = df.loc[index].apply(lambda x: x.replace("%", ""))
+        df.loc[index] = df.loc[index].apply(lambda x: x.replace(",", ""))
         df.loc[index] = df.loc[index].apply(lambda x: np.nan if x == "" else x)
         df.loc[index] = df.loc[index].apply(lambda x: float(x))
-    return df
+        df.loc[index] = df.loc[index].apply(pd.to_numeric)
+    return df.astype(float)
