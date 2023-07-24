0d6bc46270d6e42620308d9d425b314ffa8920c9
ryanalvita <ryanalvita@yahoo.com>
2023-07-24 21:18:09 +0200

    Adjust send email notification and cronjob

--- src/stock_analysis/send_email_notification.py
+++ src/stock_analysis/send_email_notification.py
@@ -1,120 +1,19 @@
 import os
-import numpy as np
 import pandas as pd
 import smtplib, ssl
 
 from datetime import datetime
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from time import sleep
-from webdriver_manager.chrome import ChromeDriverManager
-from fake_useragent import UserAgent
+from pymongo import MongoClient
 
 
 class NotifikasiEmailRilisLapkeu:
     def __init__(self):
-
-        chrome_options = webdriver.ChromeOptions()
-        # open Browser in maximized mode
-        chrome_options.add_argument("start-maximized")
-        # disabling infobars
-        chrome_options.add_argument("disable-infobars")
-        # overcome limited resource problems
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        # disabling extensions
-        chrome_options.add_argument("--disable-extensions")
-        # disabling gpu, applicable to windows os only
-        chrome_options.add_argument("--disable-gpu")
-        # bypass OS security model
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--headless")
-        # add user agent
-        chrome_options.add_argument(f"user-agent={UserAgent().random}")
-
-        self.driver = webdriver.Chrome(
-            ChromeDriverManager().install(), chrome_options=chrome_options
-        )
-
-    def get_all_releases(
-        self,
-    ):
-        # Define url
-        url = "https://idx.co.id/perusahaan-tercatat/keterbukaan-informasi/"
-
-        # Go to url
-        self.driver.get(url)
-        sleep(3)
-
-        # Fill keyword
-        self.driver.find_element(By.ID, "keywordInput").send_keys("Laporan Keuangan")
-
-        # Click search
-        period_click = self.driver.find_element(By.ID, "searchBtn")
-        self.driver.execute_script("arguments[0].click();", period_click)
-        sleep(2)
-
-        # Datetime now
-        datetime_now = datetime.now()
-        datetime_now = datetime_now.replace(hour=0, minute=0, second=0, microsecond=0)
-        datetime_yesterday = datetime_now - pd.DateOffset(1)
-
-        df = pd.DataFrame()
-
-        while True:
-            sleep(1)
-            elements = self.driver.find_elements(
-                By.XPATH, "/html/body/main/div[2]/div/div[2]/div/div"
-            )
-            i = 1
-            for element in elements:
-                texts = element.text.split("\n")
-                time = datetime.strptime(texts[0], "%d %B %Y %H:%M:%S")
-                if time < datetime_yesterday:
-                    break
-                elif time.day == datetime_yesterday.day:
-                    try:
-                        text_lapkeu = [
-                            text
-                            for text in texts
-                            if "Penyampaian Laporan Keuangan" in text
-                        ][0]
-
-                        ticker = text_lapkeu[-6:-2]
-                        text_financial = [
-                            text for text in texts if "FinancialStatement" in text
-                        ][0].split("-")
-                        quarter = text_financial[2]
-                        year = text_financial[1]
-
-                        link = self.driver.find_element(
-                            By.XPATH,
-                            f"/html/body/main/div[2]/div/div[2]/div/div[{i}]/a",
-                        ).get_attribute("href")
-
-                        df.loc[ticker, "Quarter"] = quarter
-                        df.loc[ticker, "Year"] = year
-                        df.loc[ticker, "Link"] = link
-
-                        i += 1
-                    except:
-                        i += 1
-                        continue
-
-            # Break if time is less than yesterday
-            if time < datetime_yesterday:
-                break
-            else:
-                # Continue to next page
-                period_click = self.driver.find_element(
-                    By.XPATH,
-                    "/html/body/main/div[2]/div/dir-pagination-controls/ul/li[11]/a",
-                )
-                self.driver.execute_script("arguments[0].click();", period_click)
-
-        # Store to df
-        self.df = df.reset_index()
+        # Initialize MongoDB
+        self.cluster = MongoClient(os.environ["MONGODB_URI"])
+        self.db = self.cluster["stockbit_data"]
+        self.collection = self.db["release_date"]
 
     def send_email(
         self,
@@ -125,38 +24,42 @@ class NotifikasiEmailRilisLapkeu:
         ) as f:
             html = f.read()
 
+        number = 1
+
+        dt_now = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+        dt_yesterday = dt_now - pd.DateOffset(day=1)
+
         insert = []
-
-        for ix, row in self.df.iterrows():
-            if row["Quarter"] == "I":
-                quarter = "Q1"
-            elif row["Quarter"] == "II":
-                quarter = "Q2"
-            elif row["Quarter"] == "III":
-                quarter = "Q3"
-            elif row["Quarter"] == "Tahunan":
-                quarter = "FY"
-
-            number = ix + 1
-            ticker = row["index"]
-            year = row["Year"]
-            link = row["Link"]
+        for element in self.collection.find({"latest.release_date": dt_yesterday}):
+            
+            code = element["stock_code"]
+            year = element["latest"]["year"]
+            quarter = element["latest"]["quarter"]
+            link = f"https://www.idx.co.id/id/perusahaan-tercatat/profil-perusahaan-tercatat/{code}"
             insert.append(
-                f'<tr>\n                      <td align="left" style="padding:10px;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, \'helvetica neue\', helvetica, sans-serif;line-height:27px;color:#333333;font-size:18px"><strong>{number}. {ticker} [{quarter} - {year}]</strong><br><a target="_blank" href="{link}" style="-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;text-decoration:underline;color:#1376C8;font-size:18px;font-family:arial, \'helvetica neue\', helvetica, sans-serif">Link</a></p></td>\n                     </tr>\n                     '
+                f'<tr>\n                      <td align="left" style="padding:10px;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, \'helvetica neue\', helvetica, sans-serif;line-height:27px;color:#333333;font-size:18px"><strong>{number}. {code} [{quarter} - {year}]</strong><br><a target="_blank" href="{link}" style="-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;text-decoration:underline;color:#1376C8;font-size:18px;font-family:arial, \'helvetica neue\', helvetica, sans-serif">Link</a></p></td>\n                     </tr>\n                     '
             )
 
+            number += 1
+
+        if len(insert) != 0:
+            html = html.replace(
+                '<tr>\n                      <td align="left" style="padding:10px;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, \'helvetica neue\', helvetica, sans-serif;line-height:27px;color:#333333;font-size:18px"><strong>1. BBRI [Q2 - 2022]</strong><br><a target="_blank" href="https://www.idnfinancials.com/bbri" style="-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;text-decoration:underline;color:#1376C8;font-size:18px;font-family:arial, \'helvetica neue\', helvetica, sans-serif"><u>https://www.idnfinancials.com/bbri</u></a></p></td>\n                     </tr>\n                     <tr>\n                      <td align="left" style="padding:10px;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, \'helvetica neue\', helvetica, sans-serif;line-height:27px;color:#333333;font-size:18px"><strong>2. UNVR [Q2 - 2022]</strong><br><a target="_blank" href="https://www.idnfinancials.com/unvr" style="-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;text-decoration:underline;color:#1376C8;font-size:18px;font-family:arial, \'helvetica neue\', helvetica, sans-serif">https://www.idnfinancials.com/unvr</a></p></td>\n                     </tr>\n                     <tr>\n                      <td align="left" style="padding:10px;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, \'helvetica neue\', helvetica, sans-serif;line-height:27px;color:#333333;font-size:18px"><strong>3. TLKM [Q2 - 2022]</strong><br><a target="_blank" href="https://www.idnfinancials.com/tlkm" style="-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;text-decoration:underline;color:#1376C8;font-size:18px;font-family:arial, \'helvetica neue\', helvetica, sans-serif">https://www.idnfinancials.com/tlkm</a></p></td>\n                     </tr>\n                   </table></td>\n                 </tr>\n               ',
+                "".join(insert),
+            )
+        else:
+            html = html.replace(
+                '<tr>\n                      <td align="left" style="padding:10px;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, \'helvetica neue\', helvetica, sans-serif;line-height:27px;color:#333333;font-size:18px"><strong>1. BBRI [Q2 - 2022]</strong><br><a target="_blank" href="https://www.idnfinancials.com/bbri" style="-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;text-decoration:underline;color:#1376C8;font-size:18px;font-family:arial, \'helvetica neue\', helvetica, sans-serif"><u>https://www.idnfinancials.com/bbri</u></a></p></td>\n                     </tr>\n                     <tr>\n                      <td align="left" style="padding:10px;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, \'helvetica neue\', helvetica, sans-serif;line-height:27px;color:#333333;font-size:18px"><strong>2. UNVR [Q2 - 2022]</strong><br><a target="_blank" href="https://www.idnfinancials.com/unvr" style="-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;text-decoration:underline;color:#1376C8;font-size:18px;font-family:arial, \'helvetica neue\', helvetica, sans-serif">https://www.idnfinancials.com/unvr</a></p></td>\n                     </tr>\n                     <tr>\n                      <td align="left" style="padding:10px;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, \'helvetica neue\', helvetica, sans-serif;line-height:27px;color:#333333;font-size:18px"><strong>3. TLKM [Q2 - 2022]</strong><br><a target="_blank" href="https://www.idnfinancials.com/tlkm" style="-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;text-decoration:underline;color:#1376C8;font-size:18px;font-family:arial, \'helvetica neue\', helvetica, sans-serif">https://www.idnfinancials.com/tlkm</a></p></td>\n                     </tr>\n                   </table></td>\n                 </tr>\n               ',
+                "".join(['<tr>\n                      <td align="left" style="padding:10px;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, \'helvetica neue\', helvetica, sans-serif;line-height:27px;color:#333333;font-size:12px"><center>Tidak ada laporan keuangan yang dirilis hari ini</center><br></p></td>\n                     </tr>\n                     ']),
+            )
+
+
         html = html.replace(
-            '<tr>\n                      <td align="left" style="padding:10px;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, \'helvetica neue\', helvetica, sans-serif;line-height:27px;color:#333333;font-size:18px"><strong>1. BBRI [Q2 - 2022]</strong><br><a target="_blank" href="https://www.idnfinancials.com/bbri" style="-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;text-decoration:underline;color:#1376C8;font-size:18px;font-family:arial, \'helvetica neue\', helvetica, sans-serif"><u>https://www.idnfinancials.com/bbri</u></a></p></td>\n                     </tr>\n                     <tr>\n                      <td align="left" style="padding:10px;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, \'helvetica neue\', helvetica, sans-serif;line-height:27px;color:#333333;font-size:18px"><strong>2. UNVR [Q2 - 2022]</strong><br><a target="_blank" href="https://www.idnfinancials.com/unvr" style="-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;text-decoration:underline;color:#1376C8;font-size:18px;font-family:arial, \'helvetica neue\', helvetica, sans-serif">https://www.idnfinancials.com/unvr</a></p></td>\n                     </tr>\n                     <tr>\n                      <td align="left" style="padding:10px;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, \'helvetica neue\', helvetica, sans-serif;line-height:27px;color:#333333;font-size:18px"><strong>3. TLKM [Q2 - 2022]</strong><br><a target="_blank" href="https://www.idnfinancials.com/tlkm" style="-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;text-decoration:underline;color:#1376C8;font-size:18px;font-family:arial, \'helvetica neue\', helvetica, sans-serif">https://www.idnfinancials.com/tlkm</a></p></td>\n                     </tr>\n                   </table></td>\n                 </tr>\n               ',
-            "".join(insert),
+            "1 Oktober 2020", datetime.strftime(dt_yesterday, "%d %B %Y")
         )
 
-        datetime_now = datetime.now()
-        datetime_now = datetime_now.replace(hour=0, minute=0, second=0, microsecond=0)
-        datetime_yesterday = datetime_now - pd.DateOffset(1)
-
-        html = html.replace(
-            "1 Oktober 2020", datetime.strftime(datetime_yesterday, "%d %B %Y")
-        )
+        html = html.replace('                      <td align="center" style="padding:0;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, \'helvetica neue\', helvetica, sans-serif;line-height:21px;color:#FFFFFF;font-size:14px">pahamsaham © 2022</p></td>', 
+            f'                      <td align="center" style="padding:0;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, \'helvetica neue\', helvetica, sans-serif;line-height:21px;color:#FFFFFF;font-size:14px">pahamsaham © {dt_now.year}</p></td>')
 
         # Send email
         # sender
@@ -165,7 +68,7 @@ class NotifikasiEmailRilisLapkeu:
 
         # reciever
         email_from = gmail_id
-        email_to = "ryanalvita@gmail.com"
+        email_to = gmail_id
 
         # Create message container - the correct MIME type is multipart/alternative.
         msg = MIMEMultipart("alternative")
@@ -192,9 +95,6 @@ class NotifikasiEmailRilisLapkeu:
 def main():
     """Get"""
     main_class = NotifikasiEmailRilisLapkeu()
-
-    # Get all stock code
-    main_class.get_all_releases()
 
     # Send email
     main_class.send_email()
