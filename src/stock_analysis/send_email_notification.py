dd18b79aa724aaa90b748c872af8042c2c0cdd31
ryanalvita <ryanalvita@yahoo.com>
2023-07-28 15:40:19 +0200

    Include growth to the email

--- src/stock_analysis/send_email_notification.py
+++ src/stock_analysis/send_email_notification.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from pymongo import MongoClient
+from stock_analysis.utils import clean_dataframe
 
 
 class NotifikasiEmailRilisLapkeu:
@@ -14,6 +15,7 @@ class NotifikasiEmailRilisLapkeu:
         self.cluster = MongoClient(os.environ["MONGODB_URI"])
         self.db = self.cluster["stockbit_data"]
         self.collection = self.db["release_date"]
+        self.collection_quarterly = self.db["quarterly"]
 
         self.dt_now = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
         self.dt_yesterday = self.dt_now - pd.DateOffset(days=1)
@@ -35,8 +37,35 @@ class NotifikasiEmailRilisLapkeu:
             year = element["latest"]["year"]
             quarter = element["latest"]["quarter"]
             link = f"https://www.idx.co.id/id/perusahaan-tercatat/profil-perusahaan-tercatat/{code}"
+
+            # Load data
+            data = self.collection_quarterly.find_one({"stock_code": code})
+            df = clean_dataframe(pd.DataFrame(data["income_statement"]))
+            df = df.loc[:, ::-1]
+
+            # extract the year from column names and create a year column
+            df = df.groupby(lambda x: x[-4:], axis=1).apply(lambda x: x.cumsum(axis=1))
+
+            # Shift the data by 4 periods (one year)
+            df_shifted = df.shift(4, axis=1)
+            growth = (df - df_shifted) / df_shifted * 100
+            df.loc["QoQ YTD Net Income Growth"] = growth.loc["Total Net Income Attributable To"]
+
+            for data in self.collection_quarterly.find({"stock_code": code}):
+                if data["income_statement"].get(f"{quarter} {year}"):
+                    ni_growth_ytd_yoy = round(df[f"{quarter} {year}"]["QoQ YTD Net Income Growth"], 2)
+                    if ni_growth_ytd_yoy > 0:
+                        text_color = '#008000'
+                        symbol = '▲'
+                    else:
+                        text_color = '#FF0000'
+                        symbol = '▼'
+                    growth_html = f'<br>Net Income YTD YoY: <span style="color:{text_color};"><strong>+{ni_growth_ytd_yoy}% {symbol}</strong></span></br>'
+                else:
+                    growth_html = '<br>Data is not available yet</br>'
+
             insert.append(
-                f'<tr>\n                      <td align="left" style="padding:10px;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, \'helvetica neue\', helvetica, sans-serif;line-height:27px;color:#333333;font-size:18px"><strong>{number}. {code} [{quarter} - {year}]</strong><br><a target="_blank" href="{link}" style="-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;text-decoration:underline;color:#1376C8;font-size:18px;font-family:arial, \'helvetica neue\', helvetica, sans-serif">Link</a></p></td>\n                     </tr>\n                     '
+                f'<tr>\n                      <td align="left" style="padding:10px;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, \'helvetica neue\', helvetica, sans-serif;line-height:27px;color:#333333;font-size:18px"><strong>{number}. {code} [{quarter} - {year}]</strong><br><a target="_blank" href="{link}" style="-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;text-decoration:underline;color:#1376C8;font-size:18px;font-family:arial, \'helvetica neue\', helvetica, sans-serif">Link</a></br>{growth_html}</p></td>\n                     </tr>\n                     '
             )
 
             number += 1
